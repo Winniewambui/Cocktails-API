843b47084d8e8d603c75ac23321c6c87cb342f7f
winniewambui <winniebwambui7@gmail.com>
2023-08-26 14:50:43 +0300

    modified js

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
-window.addEventListener('DOMContentLoaded',() =>{
+window.addEventListener('DOMContentLoaded', () => {
     displayCocktails();
-})
-
-// Add a "keyup" event listener to the search input
-const form = document.querySelector('#search-form')
-const inputField = form.querySelector('.input-field');
-inputField.addEventListener('keyup', function() {
-    const userInput = inputField.value.toLowerCase(); 
-
-    // Filter the drinksArray based on user input
-    let filteredCocktails = drinksArray.filter(drink => 
-        drink.strDrink.toLowerCase().includes(userInput)
-    );
-
-    // Display the filtered cocktails
-    displayCocktails(filteredCocktails);
 });
 
-function displayCocktails(){   
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a',{
-        headers:{
+// Add a "keyup" event listener to the search input
+const form = document.querySelector('#search-form');
+const inputField = form.querySelector('.input-field');
+
+function displayCocktails() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a', {
+        headers: {
             'Accept': 'application/json'
         }
     }).then((response) => {
         return response.json();
     }).then((data) => {
-        const drinksArray = data.drinks;
-      let cocktailsContainer = drinksArray.map((cocktail) =>{
-        return`
-        <article class="cocktail">
-        <img src="${cocktail.strDrinkThumb}" class="image">    
-<p class="cocktail-name">${cocktail.strDrink}</p>
-    </article>
-    `
-      })
-      document.querySelector('.cocktails-container').innerHTML = cocktailsContainer.join('')
-    })
+        const cocktailArray = data.drinks;
+        const userInput = inputField.value.toLowerCase(); 
+        
+        let filteredCocktails = cocktailArray.filter(cocktail =>
+            cocktail.strDrink.toLowerCase().includes(userInput)
+        );
+
+        // Display the filtered cocktails
+        displayFilteredCocktails(filteredCocktails); 
+
+        let cocktailsContainer = filteredCocktails.map((cocktail) => {
+            return `
+            <article class="cocktail">
+                <img src="${cocktail.strDrinkThumb}" class="image">    
+                <p class="cocktail-name">${cocktail.strDrink}</p>
+            </article>
+            `;
+        });
+
+        document.querySelector('.cocktails-container').innerHTML = cocktailsContainer.join('');
+    });
+
+    inputField.addEventListener('keyup', function() {
+        displayCocktails();
+    });
 }
 
+function displayFilteredCocktails(filteredCocktails) {
+    // Display your filtered cocktails here
+}
